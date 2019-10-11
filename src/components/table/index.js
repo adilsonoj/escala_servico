fdfcf78dc749af940d745bf12d9df6bfc53bdbe2
Adilson Oliveira <adilsonoj@yahoo.com.br>
2019-10-11 08:02:35 -0300

    criacao de calendario

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import 'moment/locale/pt-br'
+import './styles.css'
 
 export default class table extends Component {
+    geraCalendario = () =>{
+        const arr=[]
+        for(let i = 0; i<moment().daysInMonth(); i++){
+            arr.push(i)
+        }
+        return arr;
+    }
   render() {
+    console.log(moment.months())
+    console.log(moment.monthsShort())
+    console.log(moment.weekdays())
+    console.log(moment("2012-02", "YYYY-MM").daysInMonth())
+    console.log(moment().daysInMonth())
+    console.log(this.geraCalendario().forEach(el=>console.log(el)))
     return (
         <div className='container'>
-            {console.log(moment.locale())}
+        {this.geraCalendario().forEach(element => (
+            <div className="dia">segunda</div>
+            )) }
+      
+          
         </div>
     );
   }
